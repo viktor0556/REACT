caf97f3e55540bde866b519e0152fe12260cd67e
viktor0556 <134110891+viktor0556@users.noreply.github.com>
2024-07-02 13:11:11 +0200

    Create exercises.tsx

---
+++ exercises.tsx
@@ -0,0 +1,9 @@
+1. Simple component and state management:
+
+Create a React component that displays a list of books in a table.
+Create a form to add a new book that sends a POST request to the Express.js API and updates the list of books.
+
+2. API calls and data management:
+
+Create a component that displays a list of authors.
+Implement a function that allows you to delete books from the list and database with the push of a button.
