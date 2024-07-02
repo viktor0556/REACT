53e79c569cd8286d948cbe950875d95b224bb2ce
viktor0556 <134110891+viktor0556@users.noreply.github.com>
2024-07-02 13:20:48 +0200

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import DeleteBook from './DeleteFunction';
+import AuthorsList from './authors';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    < />  // Replace the blank "< />" with the component you want to try. For example <AuthorsList />
+  </React.StrictMode>
+);
+
+reportWebVitals();
+
+
